cbbc09012858666b676bd75a45f47e1ea5381811
zhouzhezz <zorichzorich@gmail.com>
2023-11-10 14:54:37 -0500

    fix

--- data/ri/FileSystem_KVStore/ri.ml
+++ data/ri/FileSystem_KVStore/ri.ml
@@ -7,3 +7,4 @@ let[@pred] rI (p : Path.t) =
   _G (Any (is_root p))
   || _G (not (Put ((p [@d]), x_1, v, true)))
   || _U (not (Put (x_0, x_1, v, p == parent x_0))) (mkdirP p)
+  || _U (not (Put ((p [@d]), x_1, v, true))) (mkdirP (parent p))
