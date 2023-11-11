b0dde1533b0ebe8ce2930a86fff236151ed4e87b
zhouzhezz <zorichzorich@gmail.com>
2023-11-11 00:15:48 -0500

    fix bug

--- data/ri/ConnectedGraph_Set/is_transition.ml
+++ data/ri/ConnectedGraph_Set/is_transition.ml
@@ -1,8 +1,6 @@
 let is_transition (st : Node.t) (en : Node.t) : bool =
-  if isNode st then
-    if isNode en then if isConnected st en then true else false else false
-  else false
+  if mem st en then true else false
 
-let[@assertRty] is_transition ((s1 : Node.t) [@ghost])
+let[@assertRty] is_transition ((s1 : Node.t) [@ghost]) ((s2 : Node.t) [@ghost])
     ?l:(st = (true : [%v: Node.t])) ?l:(en = (true : [%v: Node.t])) =
-  { pre = rI s1; res = (true : [%v: bool]); post = rI s1 }
+  { pre = rI s1 s2; res = (true : [%v: bool]); post = rI s1 s2 }
