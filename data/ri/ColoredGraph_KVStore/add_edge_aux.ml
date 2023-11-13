a90b57967cd4f1d56d3bf4eb98813140c765f4dc
zhouzhezz <zorichzorich@gmail.com>
2023-11-12 19:50:39 -0500

    fix

--- data/ri/ColoredGraph_KVStore/add_edge_aux.ml
+++ data/ri/ColoredGraph_KVStore/add_edge_aux.ml
@@ -1,12 +1,10 @@
 let add_edge_aux (st : Node.t) (en : Node.t) : unit =
-  if existsE st en then
-    let (c1 : Color.t) = getC st in
-    let (c2 : Color.t) = getC en in
-    if color_eq c1 c2 then ()
-    else (
-      putE st en;
-      ())
-  else ()
+  let (c1 : Color.t) = getC st in
+  let (c2 : Color.t) = getC en in
+  if color_eq c1 c2 then ()
+  else (
+    putE st en;
+    ())
 
 let[@assertRty] add_edge_aux ((s1 : Node.t) [@ghost]) ((s2 : Node.t) [@ghost])
     ((c : Color.t) [@ghost]) ?l:(st = (true : [%v: Node.t]))
