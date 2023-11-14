25554c563b0230563bdd3aa8d4ec339b172fdb0c
zhouzhezz <zorichzorich@gmail.com>
2023-11-14 17:06:15 -0500

    update

--- data/ri/DFA_Graph/ri.ml
+++ data/ri/DFA_Graph/ri.ml
@@ -1,9 +1,18 @@
 (* let[@pred] rI (s1 : Node.t) (a : Edge.t) (s2 : Node.t) (s3 : Node.t) = *)
 (*   implies (is_connect s1 a s2 && is_connect s1 a s3) (_G (Any (s2 == s3))) *)
 
+(* let[@pred] rI (s1 : Node.t) (a : Edge.t) = *)
+(*   not *)
+(*     (_F *)
+(*        (Connect ((s1 [@d]), (a [@d]), x_2, v, true) *)
+(*        && _X (_F (Connect ((s1 [@d]), (a [@d]), x_2, v, true))) *)
+(*        && _X (_G (not (Disconnect ((s1 [@d]), (a [@d]), x_2, v, true)))))) *)
+
 let[@pred] rI (s1 : Node.t) (a : Edge.t) =
   not
     (_F
        (Connect ((s1 [@d]), (a [@d]), x_2, v, true)
-       && _X (_F (Connect ((s1 [@d]), (a [@d]), x_2, v, true)))
-       && _X (_G (not (Disconnect ((s1 [@d]), (a [@d]), x_2, v, true))))))
+       && _X
+            (_U
+               (not (Disconnect ((s1 [@d]), (a [@d]), x_2, v, true)))
+               (Connect ((s1 [@d]), (a [@d]), x_2, v, true)))))
