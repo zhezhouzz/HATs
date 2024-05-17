d62b88bc8e50ffd4fa9961cc281c1faf60c395e9
zhouzhezz <zorichzorich@gmail.com>
2024-05-16 22:34:28 -0400

    fix minset spec

--- data/ri/MinSet_Set/minset_insert.ml
+++ data/ri/MinSet_Set/minset_insert.ml
@@ -1,14 +1,26 @@
 let minset_insert (x : Elem.t) : unit =
   if isWritten () then
     let (min : Elem.t) = read () in
-    if elem_lt x min then ()
+    if elem_lt x min then (
+      write x;
+      insert x;
+      ())
     else (
       insert x;
       ())
   else (
-    insert x;
     write x;
+    insert x;
     ())
+
+let minset_insert (x : Elem.t) : unit =
+  (if not (isWritten ()) then write x (* if empty, then update min element *)
+   else
+     let (min : Elem.t) = read () in
+     (* if not empty, read min element *)
+     if elem_lt x min then write x);
+  (* is input element is smaller, update min element *)
+  insert x (* always insert input element *)
 
 let[@assertRty] minset_insert ((m : Elem.t) [@ghost])
     ?l:(x = (true : [%v: Elem.t])) =
--- data/ri/MinSet_Set/ri.ml
+++ data/ri/MinSet_Set/ri.ml
@@ -1,5 +1,13 @@
+(* let[@pred] rI (m : Elem.t) = *)
+(*   implies (writtenP m) (minP m) *)
+(*   && implies *)
+(*        (_G (not (Write (x_0, v, true)))) *)
+(*        (_G (not (Insert (x_0, v, true)))) *)
+
 let[@pred] rI (m : Elem.t) =
-  implies (writtenP m) (minP m)
+  _G
+    (Write ((m [@d]), v, true) && _X (_G (not (Write (x_0, v, true)))))
+    #==> (minP m)
   && implies
        (_G (not (Write (x_0, v, true))))
        (_G (not (Insert (x_0, v, true))))
