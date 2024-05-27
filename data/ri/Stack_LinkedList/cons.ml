f8943981626129bbcaed44ef1f989cd7e0bb63de
Yongwei Yuan <yuan311@purdue.edu>
2024-05-27 14:19:53 -0400

    propose to fix newCell

--- data/ri/Stack_LinkedList/cons.ml
+++ data/ri/Stack_LinkedList/cons.ml
@@ -2,14 +2,10 @@ let rec cons (x : Elem.t) (s : Cell.t) : Cell.t =
   if hasPrev s then s
   else
     let (c : Cell.t) = newCell () in
-    if isCell c then
-      let (c' : Cell.t) = cons x s in
-      c'
-    else (
-      putCellContent c x;
-      setNext c s;
-      c)
+    putCellContent c x;
+    setNext c s;
+    c
 
 let[@assertRty] cons ?l:(x = (true : [%v: Elem.t]))
     ?l:(s = (true : [%v: Cell.t])) =
-  { pre = rI; res = (true : [%v: Cell.t]); post = rI }
+  { pre = rI && is_cell s; res = (true : [%v: Cell.t]); post = rI }
